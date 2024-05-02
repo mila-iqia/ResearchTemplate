b26720f72c9319544d5e1640bbb4e5960a9accb6
Fabrice Normandin <fabrice.normandin@gmail.com>
2024-05-02 14:30:28 +0000

    Remove everything
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- project/configs/__init__.py
+++
@@ -1,8 +0,0 @@
-from .datamodule import *  # noqa
-from .algorithm import *  # noqa
-from hydra_plugins.custom_launcher.custom_launcher import (
-    CustomSlurmLauncher,
-    CustomSlurmQueueConf,
-)
-
-__all__ = ["CustomSlurmLauncher", "CustomSlurmQueueConf"]
