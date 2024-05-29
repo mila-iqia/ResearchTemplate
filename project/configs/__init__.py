d3ae44487aeda30d123e699007bce4a83b054a77
Fabrice Normandin <normandf@mila.quebec>
2024-05-29 23:22:16 +0000

    Remove the custom Submitit launcher
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- project/configs/__init__.py
+++ project/configs/__init__.py
@@ -2,10 +2,6 @@ from __future__ import annotations
 
 from hydra.core.config_store import ConfigStore
 
-from hydra_plugins.custom_launcher.custom_launcher import (
-    CustomSlurmLauncher,
-    CustomSlurmQueueConf,
-)
 from project.networks import FcNetConfig, ResNet18Config
 
 from .config import Config
@@ -19,7 +15,6 @@ from .datamodule import (
     ImageNet32DataModuleConfig,
     INaturalistDataModuleConfig,
     MNISTDataModuleConfig,
-    MovingMnistDataModuleConfig,
 )
 
 # todo: look into using this instead:
@@ -32,7 +27,6 @@ cs.store(group="datamodule", name="mnist", node=MNISTDataModuleConfig)
 cs.store(group="datamodule", name="fashion_mnist", node=FashionMNISTDataModuleConfig)
 cs.store(group="datamodule", name="imagenet32", node=ImageNet32DataModuleConfig)
 cs.store(group="datamodule", name="inaturalist", node=INaturalistDataModuleConfig)
-cs.store(group="datamodule", name="moving_mnist", node=MovingMnistDataModuleConfig)
 
 
 cs.store(group="network", name="fcnet", node=FcNetConfig)
@@ -43,6 +37,4 @@ __all__ = [
     "SLURM_TMPDIR",
     "SLURM_JOB_ID",
     "REPO_ROOTDIR",
-    "CustomSlurmLauncher",
-    "CustomSlurmQueueConf",
 ]
