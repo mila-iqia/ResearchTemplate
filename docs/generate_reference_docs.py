74910d17fdb1cb5679ed44e5c69297550fc96f2c
Fabrice Normandin <normandf@mila.quebec>
2024-07-05 12:43:51 -0400

    Add a "reference" section to the documentation (#25)
    * Add a reference section with docs from the code
    
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>
    
    * Fix broken link in index.md
    
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>
    
    * Fix the docs build step.
    
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>
    
    * Skip the `build` workflow if only docs changed
    
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>
    
    * Reset pdm.lock to the same as master
    
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>
    
    ---------
    
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- docs/generate_reference_docs.py
+++ docs/generate_reference_docs.py
@@ -10,12 +10,18 @@ import mkdocs_gen_files.nav
 
 from project.utils.env_vars import REPO_ROOTDIR
 
-nav = mkdocs_gen_files.nav.Nav()
-
-
-package = "project"
-module = "project/main.py"
-submodules = ["project.datamodules", "project.utils", "project.networks", "project.algorithms"]
+module = "project"
+modules = [
+    "project/main.py",
+    "project/experiment.py",
+]
+submodules = [
+    "project.algorithms",
+    "project.configs",
+    "project.datamodules",
+    "project.networks",
+    "project.utils",
+]
 
 
 def _get_import_path(module_path: Path) -> str:
@@ -23,106 +29,56 @@ def _get_import_path(module_path: Path) -> str:
     return ".".join(module_path.relative_to(REPO_ROOTDIR).with_suffix("").parts)
 
 
-def add_doc_for_module(module_path: Path) -> None:
-    a = "reference" / (module_path.relative_to(REPO_ROOTDIR).with_suffix(".md"))
-    module_import_path = _get_import_path(module_path)
+def main():
+    nav = mkdocs_gen_files.nav.Nav()
 
-    with mkdocs_gen_files.open(a, "w") as f:
-        print(
-            textwrap.dedent(f"""\
-            ::: {module_import_path}
-            """),
-            file=f,
-        )
-    docs_dir = REPO_ROOTDIR / "docs"
-    module_path_relative_to_docs_dir = module_path.relative_to(docs_dir, walk_up=True)
-    mkdocs_gen_files.set_edit_path(a, module_path_relative_to_docs_dir)
+    add_doc_for_module(REPO_ROOTDIR / "project", nav)
+
+    # with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    #     # assert False, "\n".join(nav.build_literate_nav())
+    #     nav_file.writelines(nav.build_literate_nav())
 
 
-def get_modules(package: Path) -> list[Path]:
-    return [
+def add_doc_for_module(module_path: Path, nav: mkdocs_gen_files.nav.Nav) -> None:
+    """TODO."""
+
+    assert module_path.is_dir() and (module_path / "__init__.py").exists(), module_path
+
+    children = list(
         p
-        for p in package.glob("*.py")
-        if not p.name.endswith("_test.py") and not p.name == "__init__.py"
-    ]
+        for p in module_path.glob("*.py")
+        if not p.name.startswith("__") and not p.name.endswith("_test.py")
+    )
+    for child_module_path in children:
+        child_module_import_path = _get_import_path(child_module_path)
+        doc_file = child_module_path.relative_to(REPO_ROOTDIR).with_suffix(".md")
+        write_doc_file = f"reference/{doc_file}"
 
+        nav[tuple(child_module_import_path.split("."))] = f"{doc_file}"
 
-def get_subpackages(package: Path) -> list[Path]:
-    return [
+        with mkdocs_gen_files.open(write_doc_file, "w") as f:
+            print(
+                textwrap.dedent(f"""\
+                ::: {child_module_import_path}
+                """),
+                file=f,
+            )
+        docs_dir = REPO_ROOTDIR / "docs"
+        module_path_relative_to_docs_dir = child_module_path.relative_to(docs_dir, walk_up=True)
+        mkdocs_gen_files.set_edit_path(write_doc_file, str(module_path_relative_to_docs_dir))
+
+    submodules = list(
         p
-        for p in package.iterdir()
-        if p.is_dir() and not p.name.startswith("__") and (p / "__init__.py").exists()
-    ]
-
-
-project_nav = mkdocs_gen_files.nav.Nav()
-with mkdocs_gen_files.open("reference/project/main.md", "w") as f:
-    print(
-        textwrap.dedent("""\
-        ::: project.main
-        """),
-        file=f,
+        for p in module_path.iterdir()
+        if p.is_dir()
+        and (p / "__init__.py").exists()
+        and not p.name.endswith("_test")
+        and not p.name.startswith((".", "__"))
     )
-nav["project", "main"] = "project/main.md"
-mkdocs_gen_files.set_edit_path("reference/project/main.md", "../project/main.py")
-
-with mkdocs_gen_files.open("reference/project/experiment.md", "w") as f:
-    print(
-        textwrap.dedent("""\
-        ::: project.experiment
-        """),
-        file=f,
-    )
-nav["project", "experiment"] = "reference/project/experiment.md"
-mkdocs_gen_files.set_edit_path("reference/project/experiment.md", "../project/experiment.py")
-
-project_utils_nav = mkdocs_gen_files.nav.Nav()
-with mkdocs_gen_files.open("reference/project/utils/types.md", "w") as f:
-    print(
-        textwrap.dedent("""\
-        ::: project.utils.types
-            options:
-                show_source: true
-        """),
-        file=f,
-    )
-nav["project", "utils", "types"] = "reference/project/utils/types.md"
-mkdocs_gen_files.set_edit_path("reference/project/utils/types.md", "../project/utils/types.py")
+    for submodule in submodules:
+        add_doc_for_module(submodule, nav)
 
 
-with mkdocs_gen_files.open("reference.md", "w") as nav_file:
-    # assert False, "\n".join(nav.build_literate_nav())
-    nav_file.writelines(nav.build_literate_nav())
-
-# with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as project_nav_file:
-#     project_nav_file.writelines(project_nav.build_literate_nav())
-
-
-# project_root = REPO_ROOTDIR / "project"
-# for python_module_path in sorted(
-#     f
-#     # for f in project_root.glob("*.py")
-#     for f in [(project_root / "project")]
-#     if not f.name.endswith("_test.py") and not f.name == "__init__.py"
-# ):
-#     doc_path = python_module_path.relative_to(REPO_ROOTDIR).with_suffix(".md")
-
-#     full_doc_path = Path("reference") / doc_path
-
-#     nav[full_doc_path.with_suffix("").parts] = str(full_doc_path)
-
-#     with mkdocs_gen_files.open(full_doc_path, "w") as f:
-#         module_import_path = ".".join(
-#             python_module_path.relative_to(REPO_ROOTDIR).with_suffix("").parts
-#         )
-#         print(f"::: {module_import_path}", file=f)
-
-#     mkdocs_gen_files.set_edit_path(
-#         full_doc_path, python_module_path.relative_to(REPO_ROOTDIR / "docs", walk_up=True)
-#     )
-
-# nav["mkdocs_autorefs", "references"] = "autorefs/references.md"
-# nav["mkdocs_autorefs", "plugin"] = "autorefs/plugin.md"
-
-# with mkdocs_gen_files.open("reference.md", "w") as nav_file:
-#     nav_file.writelines(nav.build_literate_nav())
+if __name__ in ["__main__", "<run_path>"]:
+    # Run when executed directly or by mkdocs. Seems like the __name__ is <run_path> during `mkdocs serve`
+    main()
