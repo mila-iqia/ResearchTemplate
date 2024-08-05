92ce0eb68bbd9fa8b65a18d603f8375323712c5e
Fabrice Normandin <normandf@mila.quebec>
2024-08-05 19:43:24 +0000

    Remove broken test for code blocks in docstrings
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- docs/docs_test.py
+++ docs/docs_test.py
@@ -1,11 +1,7 @@
-import itertools
 import pathlib
-from inspect import getmembers
 
 import pytest
-from mktestdocs import check_docstring, check_md_file
-
-import project
+from mktestdocs import check_md_file
 
 # This retrieves all methods/properties that have a docstring.
 # todo: Brittle. We'd like something like griffe, that gets all functions / classes / etc in our module.
@@ -21,13 +17,13 @@ def get_pretty_id(obj):
 
 
 # todo: do we want to run the tests here? or do we just test the doc pages?
-@pytest.mark.parametrize(
-    "obj",
-    list(itertools.chain(map(getmembers, [project, project.configs, project.algorithms]))),
-    ids=get_pretty_id,
-)
-def test_member(obj):
-    check_docstring(obj)
+# @pytest.mark.parametrize(
+#     "obj",
+#     list(itertools.chain(map(getmembers, [project, project.configs, project.algorithms]))),
+#     ids=get_pretty_id,
+# )
+# def test_member(obj):
+#     check_docstring(obj)
 
 
 docs_folder = pathlib.Path(__file__).parent
