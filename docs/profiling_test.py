11d93cbd8d8a5cf2346e5a5e6fa5cadcfbc81037
César Miguel Valdez Córdova <cesar.valdez@mila.quebec>
2024-10-10 15:18:06 -0400

    Update docs/profiling_test.py
    Co-authored-by: Fabrice Normandin <normandf@mila.quebec>

--- docs/profiling_test.py
+++ docs/profiling_test.py
@@ -63,7 +63,11 @@ from project.utils.hydra_utils import resolve_dictconfig
         hydra.launcher.constraint="sapphire" \
         datamodule.num_workers=1,4,8,16,32
         """,
-            marks=pytest.mark.skip(reason="not working"),
+            marks=pytest.mark.xfail(
+                reason="LexerNoViableAltException error caused by the -m flag",
+                raises=hydra.errors.OverrideParseException,
+                strict=True,
+            ),
         ),
         # Identifying potential bottlenecks - fcnet mnist
         """
