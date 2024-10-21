85ff035b1e8d54a79c046b3ca8c8b0ddfa0d37ae
César Miguel Valdez Córdova <cesar.valdez@mila.quebec>
2024-10-21 16:33:50 -0400

    removed direct mock call, overrides for argv

--- project/utils/remote_launcher_test.py
+++ project/utils/remote_launcher_test.py
@@ -1,17 +1,32 @@
 # Make a Mock for the remote slurm launcher plugin
 # Use monkeypatch.setattr(project.utils.remote_launcher_plugin, ..., that_mock)
 # Assert That the mock launcher plugin was instantiated
+import sys
 from unittest.mock import Mock
 
 import pytest
 
+import project.main
+import project.utils.remote_launcher_plugin
+from project.main import main
 from project.utils.remote_launcher_plugin import RemoteSlurmLauncher
 
 
-def test_instantiate_remote_slurm_launcher_plugin(monkeypatch: pytest.MonkeyPatch):
-    launcher_mock = Mock(spec=RemoteSlurmLauncher)
-    monkeypatch.setattr("project.utils.remote_launcher_plugin.RemoteSlurmLauncher", launcher_mock)
-    launcher_mock(cluster_hostname="mila")
-    launcher_mock.assert_called_once_with(
-        cluster_hostname="mila",
+@pytest.mark.parametrize(
+    "argv",
+    [["algorithm=example", "datamodule=cifar10", "cluster=mila", "resources=one_gpu"]],
+)
+def test_instantiate_remote_slurm_launcher_plugin(
+    argv: list[str], monkeypatch: pytest.MonkeyPatch
+):
+    print(argv)
+    launcher_mock = Mock(wraps=RemoteSlurmLauncher)
+    monkeypatch.setattr(
+        project.utils.remote_launcher_plugin, RemoteSlurmLauncher.__name__, launcher_mock
     )
+
+    monkeypatch.setattr(sys, "argv", ["project/main.py"] + argv)
+    result = main()
+
+
+#    assert launcher_mock.called
