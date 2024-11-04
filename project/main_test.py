5df706ec22edf62e98c23a6b3a376cff2606115f
Fabrice Normandin <normandf@mila.quebec>
2024-11-04 16:07:01 +0000

    Add a test command to cover the `no_op` algorithm
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- project/main_test.py
+++ project/main_test.py
@@ -118,6 +118,13 @@ experiment_commands_to_test = [
         "trainer.fast_dev_run=True ",  # make each job quicker to run
         marks=pytest.mark.slow,
     ),
+    pytest.param(
+        "experiment=profiling "
+        "algorithm=no_op "
+        "datamodule=cifar10 "  # Run a small dataset instead of ImageNet (would take ~6min to process on a compute node..)
+        "trainer/logger=tensorboard "  # Use Tensorboard logger because DeviceStatsMonitor requires a logger being used.
+        "trainer.fast_dev_run=True ",  # make each job quicker to run
+    ),
 ]
 
 
