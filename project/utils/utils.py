310754d3389649b752d6a534f3709f21fdc62265
Fabrice Normandin <normandf@mila.quebec>
2024-11-18 21:14:29 +0000

    Remove more unused code
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- project/utils/utils.py
+++ project/utils/utils.py
@@ -2,49 +2,13 @@ from __future__ import annotations
 
 from collections.abc import Sequence
 from logging import getLogger as get_logger
-from pathlib import Path
-from typing import TypeVar
 
 import rich
 import rich.syntax
 import rich.tree
-import torch
-from lightning import LightningDataModule, Trainer
 from omegaconf import DictConfig, OmegaConf
 
-from project.utils.typing_utils.protocols import (
-    DataModule,
-)
-
 logger = get_logger(__name__)
-
-
-def get_log_dir(trainer: Trainer | None) -> Path:
-    """Gives back the default directory to use when `trainer.log_dir` is None (no logger used)."""
-    # TODO: This isn't great.. It could probably be a property on the Algorithm class or
-    # customizable somehow.
-    # ALSO: This
-    if trainer:
-        if trainer.logger and trainer.logger.log_dir:
-            return Path(trainer.logger.log_dir)
-        if trainer.log_dir:
-            return Path(trainer.log_dir)
-    base = Path(trainer.default_root_dir) if trainer else Path.cwd() / "logs"
-    log_dir = base / "default"
-    logger.info(
-        f"Using the default log directory of {log_dir} because a logger isn't being used."
-        # f"Consider using a logger (e.g. with 'trainer.logger=wandb' on the command-line)."
-    )
-    return log_dir
-
-
-DM = TypeVar("DM", bound=DataModule | LightningDataModule)
-
-
-# todo: shouldn't be here, should be done in `VisionDataModule` or in the configs:
-# If `normalize=False`, and there is a normalization transform in the train transforms, then an
-# error should be raised.
-# from lightning.utilities.rank_zero import rank_zero_only
 
 
 # @rank_zero_only
@@ -100,10 +64,3 @@ def print_config(
 
     # with open("config_tree.log", "w") as file:
     #     rich.print(tree, file=file)
-
-
-def default_device() -> torch.device:
-    """Returns the default device (GPU if available, else CPU)."""
-    return torch.device(
-        f"cuda:{torch.cuda.current_device()}" if torch.cuda.is_available() else "cpu"
-    )
