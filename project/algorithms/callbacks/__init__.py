b1a0cd0302e6bfea9b996cc0bdaede44d8985bb3
Fabrice Normandin <normandf@mila.quebec>
2024-12-02 13:44:48 -0500

    Remove unnecessary generic `Callback` class
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- project/algorithms/callbacks/__init__.py
+++ project/algorithms/callbacks/__init__.py
@@ -1,9 +1,7 @@
-from .callback import Callback
 from .classification_metrics import ClassificationMetricsCallback
 from .samples_per_second import MeasureSamplesPerSecondCallback
 
 __all__ = [
-    "Callback",
     "ClassificationMetricsCallback",
     "MeasureSamplesPerSecondCallback",
 ]
