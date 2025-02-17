3eda871f451833c2f3fc32839c7d4f114946bf65
Fabrice Normandin <normandf@mila.quebec>
2025-02-17 13:43:26 -0500

    Make the datamodules more "modular"/deletable
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- project/datamodules/__init__.py
+++ project/datamodules/__init__.py
@@ -2,24 +2,3 @@
 
 See the `lightning.LightningDataModule` class for more information.
 """
-
-from .image_classification import ImageClassificationDataModule
-from .image_classification.cifar10 import CIFAR10DataModule, cifar10_normalization
-from .image_classification.fashion_mnist import FashionMNISTDataModule
-from .image_classification.imagenet import ImageNetDataModule
-from .image_classification.inaturalist import INaturalistDataModule
-from .image_classification.mnist import MNISTDataModule
-from .text.text_classification import TextClassificationDataModule
-from .vision import VisionDataModule
-
-__all__ = [
-    "cifar10_normalization",
-    "CIFAR10DataModule",
-    "FashionMNISTDataModule",
-    "INaturalistDataModule",
-    "ImageClassificationDataModule",
-    "ImageNetDataModule",
-    "MNISTDataModule",
-    "VisionDataModule",
-    "TextClassificationDataModule",
-]
