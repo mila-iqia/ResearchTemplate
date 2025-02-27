7938b3a3edcdef7e9464d42fe78ada56c76894b3
Fabrice Normandin <normandf@mila.quebec>
2025-02-27 15:53:33 -0500

    Add module_name question and update tests
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,14 +1,24 @@
+import contextlib
+import dataclasses
+import shlex
 import subprocess
 import sys
+import textwrap
+from collections.abc import Sequence
 from logging import getLogger
 from pathlib import Path
+from typing import Protocol
 
 import pytest
 import tomli
+import yaml
 from copier import Worker
+from copier.vcs import get_git
+from plumbum import local
 
 import project
 import project.algorithms
+from project.main import REPO_ROOTDIR
 from project.utils.testutils import IN_GITHUB_CLOUD_CI
 
 logger = getLogger(__name__)
@@ -20,14 +30,114 @@ examples: list[str] = [
     if not (p.name == "no_op.py" or p.name.endswith("_test.py") or p.name.startswith("_"))
 ]
 
+_DEFAULT_PYTHON_VERSION = "3.10"
+"""The default choice of python version in the copier.yaml file."""
 
-@pytest.fixture(params=[None] + sorted(examples))
+_project_fixture_scope = "module"
+"""The scope of the fixtures that are used to set up the project.
+
+todo: ideally we'd only setup the project once per initial version and reuse it for all tests, but
+some tests update the folder (e.g. `test_update_project`), so we can't reuse the fixture atm.
+"""
+
+
+@pytest.fixture(
+    params=[[], *[[example] for example in examples], examples],
+    scope=_project_fixture_scope,
+    ids=["none", *examples, "all"],
+)
 def examples_to_include(request: pytest.FixtureRequest):
     """Fixture that provides the examples that would be selected by the users."""
-    example = request.param
-    # For now, we run with either no examples, or a single example from the list. We don't test
-    # combinations of examples.
-    return [] if example is None else [example]
+    # By default, select all of the examples.
+    return request.param
+
+
+def test_templated_dependencies_are_same_as_in_project():
+    """Test that the dependencies listed in the `pyproject.toml` of the template are the same as in
+    the templated pyproject.toml."""
+    project_toml = tomli.loads(Path("pyproject.toml").read_text())
+    project_toml_template = tomli.loads(Path("pyproject.toml.jinja").read_text())
+    assert sorted(project_toml["project"]["dependencies"]) == sorted(
+        project_toml_template["project"]["dependencies"]
+    )
+
+
+all_template_versions: list[str] = subprocess.getoutput("git tag --sort=-creatordate").split("\n")
+# todo: Further reduce the number of tests by only testing certain transitions (to the next major
+# or minor version for example).
+# starting_versions = [
+#     tag
+#     for tag in all_template_versions
+#     if not (v := version.parse(tag)).is_prerelease or v.is_devrelease
+# ]
+# todo: Why would we keep testing updating to older versions?
+from_to: dict[str, str] = {
+    from_version: to_version
+    for i, from_version in enumerate(all_template_versions)
+    for to_version in (all_template_versions[i + 1 :] + ["HEAD"])
+}
+
+
+@pytest.fixture(scope=_project_fixture_scope)
+def python_version(request: pytest.FixtureRequest):
+    return getattr(request, "param", _DEFAULT_PYTHON_VERSION)
+
+
+@pytest.fixture(ids=str, scope=_project_fixture_scope)
+def template_version_used(request: pytest.FixtureRequest) -> str:
+    """Return the version of the template to use when setting up the new project."""
+    return getattr(request, "param", "HEAD")
+
+
+@dataclasses.dataclass(frozen=True)
+class CopierAnswers:
+    """Answers to the questions that Copier asks the user when generating a new project."""
+
+    project_name: str = "project"
+    module_name: str = "project"
+    user_name: str = "John Doe"
+    examples_to_include: list[str] = dataclasses.field(default_factory=list)
+    github_user: str = "johndoe"
+    python_version: str = _DEFAULT_PYTHON_VERSION
+
+
+@pytest.fixture(scope=_project_fixture_scope)
+def copier_answers(
+    python_version: str,
+    examples_to_include: list[str],
+):
+    return CopierAnswers(
+        project_name="NewProject",
+        module_name="new_project",
+        user_name="John Doe",
+        examples_to_include=examples_to_include,
+        github_user="johndoe",
+        python_version=python_version,
+    )
+
+
+@pytest.fixture(scope=_project_fixture_scope)
+def project_from_template(
+    template_version_used: str,
+    tmp_path_factory: pytest.TempPathFactory,
+    copier_answers: CopierAnswers,
+):
+    """Fixture that provides the project at a given version."""
+    tmp_project_dir = tmp_path_factory.mktemp(f"project_{template_version_used}_test")
+    logger.info(
+        f"Setting up a project at {tmp_project_dir} using the template at version {template_version_used} with answers: {copier_answers}"
+    )
+    with Worker(
+        src_path="." if template_version_used == "HEAD" else "gh:mila-iqia/ResearchTemplate",
+        dst_path=tmp_project_dir,
+        vcs_ref=template_version_used,
+        defaults=True,
+        data=dataclasses.asdict(copier_answers),
+        unsafe=True,
+    ) as worker:
+        worker.run_copy()
+
+        yield worker.dst_path
 
 
 @pytest.mark.skipif(
@@ -35,6 +145,12 @@ def examples_to_include(request: pytest.FixtureRequest):
     reason="TODO: lots of issues on GitHub CI (commit author, can't install other Python versions).",
 )
 @pytest.mark.skipif(sys.platform == "win32", reason="The template isn't supported on Windows.")
+@pytest.mark.parametrize(
+    examples_to_include.__name__,
+    [[], *[[example] for example in examples], examples],
+    indirect=True,
+    ids=["none", *examples, "all"],
+)
 @pytest.mark.parametrize(
     "python_version",
     [
@@ -57,58 +173,191 @@ def examples_to_include(request: pytest.FixtureRequest):
             ],
         ),
     ],
+    indirect=True,
 )
-def test_template(
-    examples_to_include: list[str],
-    python_version: str,
+def test_setup_project(
+    project_from_template: Path,
+    copier_answers: CopierAnswers,
+    template_version_used: str,
     tmp_path: Path,
-    monkeypatch: pytest.MonkeyPatch,
 ):
     """Run Copier programmatically to test the the setup for new projects.
 
     NOTE: This test is slow at first, as it might fill up your UV cache with torch / jax / etc for
     each python version!
     """
-    tmp_project_dir = tmp_path / "new_project"
+    project_root = project_from_template
+    # Check that the answers were saved correctly.
+    answers_file = project_root / ".copier-answers.yml"
+    assert answers_file.exists()
+    copier_content = yaml.safe_load(answers_file.read_text())
+    assert all(copier_content[k] == v for k, v in dataclasses.asdict(copier_answers).items())
 
-    # This is like doing `copier copy --trust gh:mila-iqia/ResearchTemplate new_project`
-    # Except we use `--vcs-ref HEAD` and supply the answers (`-d project_name=new_project ...`)
-
-    with Worker(
-        src_path=".",
-        dst_path=tmp_project_dir,
-        vcs_ref="HEAD",
-        defaults=True,
-        data={
-            "project_name": "new_project",
-            "user_name": "John Doe",
-            "examples_to_include": examples_to_include,
-            "github_user": "johndoe",
-            "python_version": python_version,
-        },
-        unsafe=True,
-    ) as worker:
-        worker.run_copy()
-        # Note: here we just collect tests.
-        command = ["uv", "run", "pytest", "-v", "--collect-only"]
-        logger.info(f"Running: command `{' '.join(command)}`")
-        result = subprocess.run(
-            command,
-            cwd=tmp_project_dir,
-            text=True,
-            capture_output=True,
-        )
-        if result.returncode != 0:
-            print("STDOUT: ", result.stdout)
-            print("STDERR: ", result.stderr)
-            pytest.fail(f"Failed to collect tests in the new project: {result.stdout}")
+    # Check that tests can be collected without errors. This is usually a good "smoke" test to
+    # check for package import errors and such.
+    command = ["uv", "run", "pytest", "-v", "--collect-only"]
+    logger.info(f"Running: command `{' '.join(command)}`")
+    result = subprocess.run(command, cwd=project_root, text=True, capture_output=True)
+    if result.returncode != 0:
+        print("STDOUT: ", result.stdout)
+        print("STDERR: ", result.stderr)
+        pytest.fail(f"Failed to collect tests in the new project: {result.stdout}")
 
 
-def test_templated_dependencies_are_same_as_in_project():
-    """Test that the dependencies listed in the `pyproject.toml` of the template are the same as in
-    the templated pyproject.toml."""
-    project_toml = tomli.loads(Path("pyproject.toml").read_text())
-    project_toml_template = tomli.loads(Path("pyproject.toml.jinja").read_text())
-    assert sorted(project_toml["project"]["dependencies"]) == sorted(
-        project_toml_template["project"]["dependencies"]
+class ModifyAndTest(Protocol):
+    """
+    IDEA:
+    1. Start from any of the published versions of the template (including the current (HEAD) version).
+    2. Apply some modifications from a list of modifications, e.g. adding a file or something
+    3. Yield. If the initial version is not HEAD, `copier update` is run, accepting to overwrite
+       anything that is different.
+    4. Run a test to check that the project created from the template works as expected.
+    """
+
+    def __call__(
+        self, project_root: Path, answers: CopierAnswers, template_version_used: str
+    ) -> contextlib._GeneratorContextManager[None, None, None]: ...
+
+
+@contextlib.contextmanager
+def add_lit_autoencoder_module(
+    project_root: Path, answers: CopierAnswers, template_version_used: str
+):
+    """Add a module to the project."""
+    # todo: This 'module_name' was added after v0.0.1
+    project_module = (
+        answers.project_name if template_version_used == "v0.0.1" else answers.module_name
     )
+    new_module = project_root / project_module / "algorithms" / "lit_autoencoder.py"
+    new_module.write_text(
+        textwrap.dedent("""\
+        import os
+        from torch import optim, nn, utils, Tensor
+        from torchvision.datasets import MNIST
+        from torchvision.transforms import ToTensor
+        import lightning as L
+
+
+        # define the LightningModule
+        class LitAutoEncoder(L.LightningModule):
+            def __init__(self):
+                super().__init__()
+                # define any number of nn.Modules (or use your current ones)
+                self.encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+                self.decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
+
+            def training_step(self, batch, batch_idx):
+                # training_step defines the train loop.
+                # it is independent of forward
+                x, _ = batch
+                x = x.view(x.size(0), -1)
+                z = self.encoder(x)
+                x_hat = self.decoder(z)
+                loss = nn.functional.mse_loss(x_hat, x)
+                # Logging to TensorBoard (if installed) by default
+                self.log("train_loss", loss)
+                return loss
+
+            def configure_optimizers(self):
+                optimizer = optim.Adam(self.parameters(), lr=1e-3)
+                return optimizer
+        """)
+    )
+    new_algo_config = (
+        project_root / project_module / "configs" / "algorithm" / "lit_autoencoder.yaml"
+    )
+    # Add a Hydra config file for the new module.
+    new_algo_config.write_text(
+        textwrap.dedent(f"""\
+        _target_: {project_module}.algorithms.lit_autoencoder.LitAutoEncoder
+        """)
+    )
+
+    yield  # Yield, (let the project update if needed)
+
+    # Then test that it still works:
+
+    command = (
+        f"uv run python {answers.module_name}/train.py "
+        f"algorithm=lit_autoencoder datamodule=mnist trainer.fast_dev_run=True"
+    )
+
+    logger.info(f"Running: command `{command}` in the new, modified project")
+    result = subprocess.run(shlex.split(command), cwd=project_root, text=True, capture_output=True)
+    if result.returncode != 0:
+        print("STDOUT: ", result.stdout)
+        print("STDERR: ", result.stderr)
+        pytest.fail(f"Failed to collect tests in the new project: {result.stdout}")
+
+
+# @contextlib.contextmanager
+# def no_changes(project_root: Path, answers: CopierAnswers, template_version_used: str):
+#     yield
+
+
+modifications: Sequence[ModifyAndTest] = [
+    add_lit_autoencoder_module,
+    # no_changes,
+]
+
+
+@pytest.fixture(scope="module")
+def version_to(request: pytest.FixtureRequest):
+    return getattr(request, "param", "HEAD")
+
+
+# @pytest.mark.skip(reason="TODO: This test is still a work in progress, doesn't exactly work yet.")
+@pytest.mark.skipif(
+    IN_GITHUB_CLOUD_CI,
+    reason="TODO: lots of issues on GitHub CI (commit author, can't install other Python versions).",
+)
+@pytest.mark.skipif(sys.platform == "win32", reason="The template isn't supported on Windows.")
+@pytest.mark.parametrize(
+    (template_version_used.__name__, version_to.__name__), sorted(from_to.items()), indirect=True
+)
+@pytest.mark.parametrize("modification", modifications)
+def test_update_project(
+    # project_from_template: Path,  # can't use a shared fixture since we modify the project.
+    copier_answers: CopierAnswers,
+    modification: ModifyAndTest,
+    template_version_used: str,
+    version_to: str,
+    tmp_path: Path,
+):
+    """IDEA: Test that sets up a project from the template at a given version and then updates it
+    to new version with `copier update`.
+    """
+    project_root = tmp_path / "temp_project"
+    data_file = tmp_path / "copier_inputs.yaml"
+    data_file.write_text(yaml.dump(dataclasses.asdict(copier_answers)))
+
+    # https://plumbum.readthedocs.io/en/latest/#cheat-sheet
+    copier_command = local["uvx"]["copier"]
+    copier_command[
+        "copy",
+        "--trust",
+        "--defaults",
+        "--data-file",
+        str(data_file),
+        f"--vcs-ref={template_version_used}",
+        str(REPO_ROOTDIR),
+        str(project_root),
+    ]()
+
+    with modification(project_root, copier_answers, template_version_used):
+        # Need to commit changes for copier to accept to update.
+        git = get_git().with_cwd(project_root)
+        # TODO: Double-check that these changes only affect the temporary cloned repo, not this
+        # current dev repo.
+        git("config", "commit.gpgsign", "false")
+        local["pre-commit"].with_cwd(project_root)("uninstall")
+        git("add", ".")
+        git("commit", "-m", f"Apply modifications from {_nameof(modification)}.")
+
+        copier_command.with_cwd(project_root)["update"](
+            "--trust", "--defaults", "--vcs-ref", version_to
+        )
+
+
+def _nameof(thing) -> str:
+    return getattr(thing, "__name__", str(thing))
